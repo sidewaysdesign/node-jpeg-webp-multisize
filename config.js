8128820b57a96d2911284946b20b15a52b433298
sidewaysdesign <richard@sidewaysdesign.com>
2020-10-19 11:41:20 -0400

    Added comment in config re: 0 in sizes array

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {}
-config.sizes = [0, 360, 600, 1024]
+config.sizes = [0, 360, 600, 1024] /* include 0 value to copy files at original size */
 config.source = 'images'
 config.destination = 'output'
 config.quality = { webp: 50, jpeg: 60 }
